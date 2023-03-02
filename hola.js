e38b38a3419644b614a16a7fc0c9636f05f14cd5
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2023-03-02 12:16:56 -0600

    Video 7 terminado tipos primitivos

--- hola.js
+++ hola.js
@@ -1,1 +1,16 @@
-console.log('hola mundo');
+function saludar(nombre) {
+    return "Hola ".concat(nombre.toUpperCase());
+}
+console.log(saludar('luis'));
+//**************************TIPOS*************************
+//boolean
+var isVerdadero = false;
+//number
+var entero = 44;
+var decimal = 20.05;
+var hexadecimal = 0xcbcbaa;
+var octal = 0700;
+var binario = 010000001;
+//puedes usar guion bajo para leerlo facilmente
+var numberoLargo = 7100288;
+console.log(numberoLargo);
--- hola.ts
+++ hola.ts
@@ -1,1 +1,36 @@
-console.log('hola mundo');
+function saludar(nombre: string): string {
+  return `Hola ${nombre.toUpperCase()}`;
+}
+
+console.log(saludar('luis'));
+//**************************TIPOS PRIMITIVOS*************************
+//boolean
+let isVerdadero: boolean = false;
+//number
+let entero: number = 44;
+let decimal = 20.05;
+let hexadecimal: number = 0xcbcbaa;
+let octal: number = 0700;
+let binario: number = 010000001;
+//puedes usar guion bajo para leerlo facilmente
+let numberoLargo: number = 7_100_288;
+console.log(numberoLargo);
+//string
+let saludo = 'hola';
+
+//******************TIPOS PRIMITIVOS DE TYPESCRIPT*************************
+//any
+// toman cualquier tipo de valor, no usar nunca;
+let unaVariable;
+unaVariable = 'hola';
+unaVariable = false;
+
+//null
+//Solo puedes asignar null
+let nada: null = null;
+
+//undefined
+let indefinido: undefined = undefined;
+
+//void
+const unaFuncion = (): void => console.log('ejecutando');
